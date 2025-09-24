2ab308cd68d9d70b4e09b4a0537569e97084e2c5
velem <mlulekivelem@gmail.com>
2025-09-24 02:59:37 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+// frontend/src/App.jsx
+
+import { Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Register />} />
+    </Routes>
+  );
+}
+
+export default App;
+
+
